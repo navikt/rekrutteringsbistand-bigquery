68409c9e1f42f02cf3051807da9f3d6e3e434f29
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2024-07-16 09:07:59 +0200

    Kjørte "black", en python-formatter

--- speiling_db_bq.py
+++ speiling_db_bq.py
@@ -5,7 +5,7 @@ import psycopg2 as pg
 import pandas.io.sql as psql
 import logging
 
-logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(encoding="utf-8", level=logging.DEBUG)
 logger = logging.getLogger("speiling_db_bq.py")
 
 try:
@@ -16,11 +16,14 @@ except:
 
 try:
     bigQueryKlientNøkkel = secrets.pop("GCP_json")
-    bigQueryCredentials = service_account.Credentials.from_service_account_info(eval(bigQueryKlientNøkkel))
+    bigQueryCredentials = service_account.Credentials.from_service_account_info(
+        eval(bigQueryKlientNøkkel)
+    )
     bigQueryKlient = bigquery.Client(credentials=bigQueryCredentials)
 except:
     logger.error("Kunne ikke lage BigQuery-klient")
     exit(1)
+
 
 # Funksjon brukt for å speile tabeller fra en database til BigQuery
 def speiling_db_bq(db_navn, tabeller, bigQueryKlient, logger):
@@ -28,7 +31,9 @@ def speiling_db_bq(db_navn, tabeller, bigQueryKlient, logger):
         creds = secrets[db_navn + "-db-url"]
         adeo, _, creds_loc = creds.split(":")
         user, password = vault_api.get_database_creds(creds_loc).split(":")
-        connection = pg.connect(f"host={adeo} dbname={db_navn} user={user} password={password}")
+        connection = pg.connect(
+            f"host={adeo} dbname={db_navn} user={user} password={password}"
+        )
     except:
         logger.error(f"Kunne ikke opprette databaseklient for {db_navn}")
         exit(1)
@@ -37,20 +42,25 @@ def speiling_db_bq(db_navn, tabeller, bigQueryKlient, logger):
         try:
             sql = "select * from " + tabell
             dataframe = psql.read_sql(sql, connection)
-            dataframe.columns = dataframe.columns.str.replace({"å": "aa", "ø": "o", "æ": "ae"}, regex=True)
-            jobConfig = bigquery.LoadJobConfig(schema=tabellKonfigurasjon, write_disposition="WRITE_TRUNCATE")
+            dataframe.columns = dataframe.columns.str.replace(
+                {"å": "aa", "ø": "o", "æ": "ae"}, regex=True
+            )
+            jobConfig = bigquery.LoadJobConfig(
+                schema=tabellKonfigurasjon, write_disposition="WRITE_TRUNCATE"
+            )
             job = bigQueryKlient.load_table_from_dataframe(
                 dataframe,
                 f"toi-prod-324e.{db_navn.replace('-', '_')}.{tabell}",
-                job_config=jobConfig
+                job_config=jobConfig,
             )
             job.result()
             logger.info(f"Har speilet tabell {tabell} til BigQuery")
         except:
             logger.error(f"Kunne ikke speile tabell {tabell} til BigQuery")
             exit(1)
-        
+
         logger.error(f"Ferdig med speiling av tabeller fra {db_navn}")
+
 
 # Speiling av kandidat-API
 tabeller = {
